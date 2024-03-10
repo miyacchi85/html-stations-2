b647965ac2cc439ac26851c20a3abd593d05b437
miyacchi85 <miyahira@skyarch.net>
2024-03-10 14:14:52 +0900

    clear_station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,11 @@
+const checkbox = document.getElementById("check");
+const text = document.getElementById("text");
+
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  if (checkbox.checked) {
+    text.style.backgroundColor = "red";
+  } else {
+    text.style.backgroundColor = "transparent";
+  }
 }
